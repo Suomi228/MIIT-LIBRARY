99a1fb90f5ec4bc9e0a9fe20b0016a8a7fb57631
1.kla$ <recum12233@mail.ru>
2024-04-06 17:34:04 +0300

    чуть чуть

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,27 +2,31 @@ import {Injectable} from '@angular/core';
 import {UserLogin} from "./user";
 import {JwtToken} from "./jwtToken";
 import {HttpClient} from "@angular/common/http";
+import {BASE_API_URL} from "../constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) {   }
+  constructor(private http: HttpClient) {
+  }
 
-  login(user: UserLogin) {
-    return this.http.post<JwtToken>('http://localhost:8080/api/v1/auth/authentication', user);
+  private postNoAuth(packet: any, endpoint: string) {
+    return this.http.post<JwtToken>(BASE_API_URL + endpoint, packet);
   }
 
   submitApplication(email: string, password: string) {
     console.log(`Homes application received: email: ${email}, password: ${password}.`);
-    this.login(new UserLogin(email, password)).subscribe({
+    this.postNoAuth(new UserLogin(email, password), 'auth/authentication')
+      .subscribe({
       next: (token) => {
         console.log(`Token received: ${token.token}`);
       },
       error: (error) => {
-        console.log(`Error: ${error}`);
+        // выводит код ошибки, тут обычно 403 если неправильный логин или пароль
+        console.log(`Error: ${error.status}`);
       }
     });
-    }
   }
+}
