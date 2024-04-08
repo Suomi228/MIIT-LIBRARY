aba82b7d1de6387aad24bc461c16523c3b58ccd7
Suomi228 <bixep345@gmail.com>
2024-04-08 22:18:20 +0300

    genereted header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 
 import { LoginComponent } from './login/login.component';
 import {RouterOutlet} from "@angular/router";
+import { HomePageComponent } from './home-page/home-page.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomePageComponent,CatalogueComponent,HeaderComponent],
   template: `
   <main class="body">
+  <app-header></app-header>
     <section class="content">
+    
 <!--      <app-login></app-login>-->
-      <router-outlet></router-outlet>
+      <!-- <app-home-page></app-home-page> -->
+      <!-- <app-catalogue></app-catalogue> -->
     </section>
   </main>
 `,
