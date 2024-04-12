2543a363d0214f00d0caacd78be2e0e47eb5d99d
Suomi228 <bixep345@gmail.com>
2024-04-12 21:57:47 +0300

    LoanRequest

--- src/app/pass-book/pass-book.component.ts
+++ src/app/pass-book/pass-book.component.ts
@@ -1,15 +1,24 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { BorrowedBooksService } from '../services/borrowed-books.service';
-
+import { HttpClientModule } from '@angular/common/http';
+import { CreateLoanResponse } from '../catalogue/requests/createLoanRequest';
 @Component({
   selector: 'app-pass-book',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,HttpClientModule],
   templateUrl: './pass-book.component.html',
-  styleUrl: './pass-book.component.scss'
+  styleUrl: './pass-book.component.scss',
+  providers: [BorrowedBooksService]
 })
 export class PassBookComponent {
 
-    constructor(public bBooks: BorrowedBooksService){}
+   private bBookArray: any =[];
+   constructor(@Inject(BorrowedBooksService) private bBook: BorrowedBooksService, private loan: CreateLoanResponse){}
+  //  ngOnInit() {
+  //   this.bBook.getLoan().subscribe(data =>{
+  //     this.bBookArray.push(data);
+  //     console.log(this.bBookArray)
+  //   })
+  // }
 }
--- src/app/services/borrowed-books.service.ts
+++ src/app/services/borrowed-books.service.ts
@@ -1,15 +1,36 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-
+import { Observable } from 'rxjs';
+import { GetBookResponse } from '../catalogue/requests/getBookRequest';
+import {BASE_API_URL} from "../constants";
+import { CreateLoanResponse } from '../catalogue/requests/createLoanRequest';
 @Injectable({
   providedIn: 'root'
 })
 export class BorrowedBooksService {
 
-  constructor() { }
-  bBooks: string [] = [];
-  
-  addBook(book: string){
-    this.bBooks.push(book);
+  constructor(private http: HttpClient) { }
+
+  addBook(){
+    let zalupa=new CreateLoanResponse(1);
+    console.log(zalupa.bookId);
+    this.createLoan(zalupa).subscribe({
+      next: (token) => {
+        console.log("tts");
+      },
+      error: (error) => {
+        // выводит код ошибки, тут обычно 403 если неправильный логин или пароль
+        console.log(`Error: ${error.status}`);
+      }
+    });;
+  }
+  createLoan(createLoanResponse: CreateLoanResponse): Observable<{ books: GetBookResponse[] }> {
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'));
+    console.log("created loan")
+    return this.http.post<{ books: GetBookResponse[] }>(BASE_API_URL + "loan/create",createLoanResponse, {headers: headers});
+  }
+  getLoan(packet: any, endpoint: string) {
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'));
+    return this.http.get(BASE_API_URL + endpoint, {headers: headers});
   }
 }
