f6b93542ade13e48226324bfffa26c0de04a1dfa
Suomi228 <bixep345@gmail.com>
2024-04-26 11:18:05 +0300

    catalogue-detail design

--- src/app/catalogue-detail/catalogue-detail.component.ts
+++ src/app/catalogue-detail/catalogue-detail.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { GetBookResponse } from '../catalogue/requests/getBookRequest';
 import { BookService } from '../services/book.service';
+import {MatCardContent, MatCardModule, MatCardTitle} from "@angular/material/card";
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-catalogue-detail',
   standalone: true,
-  imports: [RouterModule,HttpClientModule],
+  imports: [RouterModule,HttpClientModule,MatCardContent,MatCardModule],
   templateUrl: './catalogue-detail.component.html',
   styleUrl: './catalogue-detail.component.scss',
   providers: [BookService]
