3c6bf836b2c351310412f97b895f204ad51104b4
Suomi228 <bixep345@gmail.com>
2024-05-03 16:16:24 +0300

    filtered header (need fix)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,17 +2,38 @@ import {Component} from '@angular/core';
 import {Router, RouterModule} from '@angular/router';
 import {HomePageComponent} from '../home-page/home-page.component';
 import {CatalogueComponent} from '../catalogue/catalogue.component';
+import { NAV_BAR } from '../constants';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, HomePageComponent, CatalogueComponent],
+  imports: [RouterModule, HomePageComponent, CatalogueComponent,NgFor,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+// NEW COMMIT
+  menus:any [] = [];
+  filteredMenus:any [] = [];
+  role: any = '';
   constructor(private router: Router) {
+    this.menus = NAV_BAR.menus;
+    const token = localStorage.getItem('token')
+    if (token!= null){
+        this.role = localStorage.getItem('role')
+    }
+
+    this.menus.forEach((element: any)=>{
+      const userRole = element.roles.some((role: any)=>role == this.role)
+      console.log(userRole)
+      if (userRole!= undefined){
+        this.filteredMenus.push(element);
+      }
+    });
+
   }
+// NEW COMMIT
 
   isRoute(route: string) {
     return this.router.url === route;
